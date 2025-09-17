5924ab6d0a613c9c0b9758ced66a86b7d9a94315
Mraprguild8133 <mraprguild06@gmail.com>
2025-09-17 12:40:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ API_HASH = os.environ.get("API_HASH", "your_api_hash") # Your API_HASH
 # Get this from @BotFather on Telegram
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token") # Your Bot Token
 
-Replace 123456789 with your actual Telegram User ID from @userinfobot.
 # If you have multiple users, separate the numbers with commas inside the curly braces.
 # For example: AUTH_USERS = {123456789, 987654321}
 AUTH_USERS = {6300568870}
