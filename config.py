3c982ef99818992574b3c745fbcb23a036f0308b
Mraprguild8133 <mraprguild06@gmail.com>
2025-09-17 19:21:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import os
 
-# Get these values from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "123456")) # Your API_ID
-API_HASH = os.environ.get("API_HASH", "your_api_hash") # Your API_HASH
+class Config(object):
+    # Get these values from my.telegram.org
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH", "")
 
-# Get this from @BotFather on Telegram
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token") # Your Bot Token
+    # Get this from @BotFather on Telegram
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-# If you have multiple users, separate the numbers with commas inside the curly braces.
-# For example: AUTH_USERS = {123456789, 987654321}
-AUTH_USERS = {6300568870}
-# Directory to save downloaded files temporarily.
-DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads/")
+    # The Telegram user ID of the admin
+    ADMIN = int(os.environ.get("ADMIN", 12345))
+
+    # Directory for downloads
+    DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
